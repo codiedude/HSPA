08fe9dc31ac28cb5dddcd84ee9e7e114fec88eb4
Sudheer <sudheer2scorpio@gmail.com>
2024-04-23 23:24:27 +0530

    gs

--- Frontend/src/app/property-list/property-list.component.ts
+++ Frontend/src/app/property-list/property-list.component.ts
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrl: './property-list.component.css'
 })
 export class PropertyListComponent {
+  Property:Array <any>=[
+    {
+       "Id":1,
+       "Name":"Birla House",
+       "Type":"House",
+       "Price":12000
 
+    },
+{
+   "Id":2,
+   "Name":"Erose villa",
+   "Type":"House",
+   "Price":13000
+
+},
+{
+   "Id":3,
+   "Name":"NacroHome",
+   "Type":"House",
+   "Price":15666
+  }
+  ,
+  {
+     "Id":4,
+     "Name":"PerlHome",
+     "Type":"House",
+     "Price":15666
+    }
+    ,
+    {
+       "Id":3,
+       "Name":"Perl White",
+       "Type":"House",
+       "Price":15666
+      }
+  ]
 }
